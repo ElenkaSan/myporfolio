90d781f3f16e82a18e3b32d134f3bf8c63360962
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-10-17 13:49:24 -0400

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,10 +11,10 @@ export const ThemeContext = createContext(null)
 
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
 
   const themeToggler = () => {
-    setTheme((curr)=> (curr === "light" ? "dark" : "light"))
+    setTheme((curr)=> (curr === "dark" ? "light" : "dark"))
     // theme === 'dark' ? setTheme('light') : setTheme('dark');
   };
 
